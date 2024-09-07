76d4eb7950973bd0bcadb8a60b515a758f782468
saikiran6789 <122426288+saikiran6789@users.noreply.github.com>
2024-09-07 20:02:09 +0530

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+import java.util.Date;
+
+public class Transaction {
+    private String type;
+    private double amount;
+    private Date date;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.date = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return type + ": $" + amount + " on " + date;
+    }
+}
+
+
